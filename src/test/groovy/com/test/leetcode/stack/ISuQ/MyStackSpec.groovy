7020c75597a9726ee4f8c1f9438e3c692697133c
keshavendra <kpsingh2607@gmail.com>
2023-08-29 23:37:00 +0530

    Added Spock test cases.

---
+++ src/test/groovy/com/test/leetcode/stack/ISuQ/MyStackSpec.groovy
@@ -0,0 +1,88 @@
+package com.test.leetcode.stack.ISuQ
+
+import spock.lang.Specification
+
+class MyStackSpec extends Specification {
+    MyStack stack
+    int[] listOfElements
+
+    def setup() {
+        stack = Spy(MyStack)
+    }
+
+    def "Test push operation"() {
+        given:
+        listOfElements = new int[3]
+        listOfElements[0] = 1
+        listOfElements[1] = 2
+        listOfElements[2] = 3
+
+        when:
+        for(int i=0; i<listOfElements.length;i++)
+            stack.push(listOfElements[i])
+
+        then:
+        listOfElements.length * stack.push(_)
+
+    }
+
+    def "Test pop operation"() {
+        given:
+        listOfElements = new int[3]
+        listOfElements[0] = 1
+        listOfElements[1] = 2
+        listOfElements[2] = 3
+        int popValue
+
+        when:
+        for(int i=0; i<listOfElements.length;i++)
+            stack.push(listOfElements[i])
+        popValue = stack.pop()
+
+        then:
+        popValue == listOfElements[listOfElements.length-1]
+        1 * stack.pop()
+        listOfElements.length * stack.push(_)
+    }
+
+    def "Test top operation"() {
+        given:
+        listOfElements = new int[3]
+        listOfElements[0] = 1
+        listOfElements[1] = 2
+        listOfElements[2] = 3
+        int topValue
+
+        when:
+        for(int i=0; i<listOfElements.length;i++)
+            stack.push(listOfElements[i])
+        topValue = stack.top()
+
+        then:
+        topValue == listOfElements[listOfElements.length-1]
+        0 * stack.pop()
+        1 * stack.top()
+        listOfElements.length * stack.push(_)
+    }
+
+    def "Test empty operation"() {
+        given:
+        listOfElements = new int[3]
+        listOfElements[0] = 1
+        listOfElements[1] = 2
+        listOfElements[2] = 3
+        boolean isEmpty
+
+        when:
+        for(int i=0; i<listOfElements.length;i++)
+            stack.push(listOfElements[i])
+        isEmpty = stack.empty()
+
+        then:
+        !isEmpty
+        0 * stack.pop()
+        0 * stack.top()
+        1 * stack.empty()
+        listOfElements.length * stack.push(_)
+    }
+}
