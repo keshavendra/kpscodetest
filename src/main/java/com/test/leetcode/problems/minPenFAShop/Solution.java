f5fafc089973e3f8418fd538880d1b3d2ae62baa
keshavendra <kpsingh2607@gmail.com>
2023-08-30 01:25:10 +0530

    Removed main method.

--- src/main/java/com/test/leetcode/problems/minPenFAShop/Solution.java
+++ src/main/java/com/test/leetcode/problems/minPenFAShop/Solution.java
@@ -3,17 +3,6 @@ package com.test.leetcode.problems.minPenFAShop;
 
 public class Solution {
 
-    public static void main(String[] args) {
-
-        String str1 = "YYNY";
-        String str2 = "NNNNN";
-        String str3 = "YYYY";
-        Solution sol = new Solution();
-        System.out.println("for " + str1 + "\tExpected is 2, actual is " + sol.bestClosingTime(str1));
-        System.out.println("for " + str2 + "\tExpected is 0, actual is " + sol.bestClosingTime(str2));
-        System.out.println("for " + str3 + "\tExpected is 4, actual is " + sol.bestClosingTime(str3));
-    }
-
     public int bestClosingTime(String customers) {
         int totalYCountInRight = 0;
         for (int i = 0; i < customers.length(); i++) {
