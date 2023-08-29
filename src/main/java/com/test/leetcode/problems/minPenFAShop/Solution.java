b0b520c9c7c80f26c65d5c75827854912519ce5a
keshavendra <kpsingh2607@gmail.com>
2023-08-30 01:14:04 +0530

    Updated pom.xml. Added more problem with solution

---
+++ src/main/java/com/test/leetcode/problems/minPenFAShop/Solution.java
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/minimum-penalty-for-a-shop/
+package com.test.leetcode.problems.minPenFAShop;
+
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        String str1 = "YYNY";
+        String str2 = "NNNNN";
+        String str3 = "YYYY";
+        Solution sol = new Solution();
+        System.out.println("for " + str1 + "\tExpected is 2, actual is " + sol.bestClosingTime(str1));
+        System.out.println("for " + str2 + "\tExpected is 0, actual is " + sol.bestClosingTime(str2));
+        System.out.println("for " + str3 + "\tExpected is 4, actual is " + sol.bestClosingTime(str3));
+    }
+
+    public int bestClosingTime(String customers) {
+        int totalYCountInRight = (int) (IntStream.range(0, customers.length()).filter(x -> customers.charAt(x)=='Y')).count();
+        int totalNCountInLeft = 0;
+        int minPenalty = totalYCountInRight;
+        int closingIndex = 0;
+        for (int i = 0; i < customers.length(); i++) {
+            char currentChar = customers.charAt(i);
+            //if we close at the current index what will be penalty now?
+            if (totalYCountInRight + totalNCountInLeft < minPenalty) {
+                minPenalty = totalYCountInRight + totalNCountInLeft;
+                closingIndex = i;
+            }
+            if (currentChar == 'N')
+                totalNCountInLeft++;
+            else
+                totalYCountInRight--;
+        }
+        //one final attempt
+        if (totalYCountInRight + totalNCountInLeft < minPenalty) {
+            closingIndex = customers.length();
+        }
+        return closingIndex;
+    }
+}
