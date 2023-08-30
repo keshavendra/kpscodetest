b5609c4b2ece92db08d7189f4f797e084df7f63a
keshavendra <kpsingh2607@gmail.com>
2023-08-30 20:11:38 +0530

    Added ListNode as utils package class.

---
+++ src/main/java/com/testds/leetcode/utils/ListNode.java
@@ -0,0 +1,13 @@
+package com.testds.leetcode.utils;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
