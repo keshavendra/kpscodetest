f3b542b25780d720afef7c3df79bbed6e986087f
keshavendra <kpsingh2607@gmail.com>
2023-09-06 00:52:24 +0530

    Added code for 'copy-list-with-random-pointer' problem.

---
+++ src/test/groovy/com/testds/leetcode/problems/copyListWithRandomPointer/Solution2Spec.groovy
@@ -0,0 +1,23 @@
+package com.testds.leetcode.problems.copyListWithRandomPointer
+
+import spock.lang.Specification
+
+class Solution2Spec extends Specification {
+    Solution2 solution
+
+    def setup() {
+        solution = new Solution2()
+    }
+
+    def "test copy list with random pointers"() {
+        given:
+        def originalList = Solution1.createList(nodeArray as Integer[][])
+        when:
+        def copiedList = solution.copyRandomList(originalList)
+        then:
+        Solution1.nodeToListofListOfIntegers(copiedList) == nodeArray as Integer[][]
+        where:
+        nodeArray << [[[7, null], [13, 0], [11, 4], [10, 2], [1, 0]], [[1, 1], [2, 1]], [[3, null], [3, 0], [3, null]],[[]]]
+
+    }
+}
