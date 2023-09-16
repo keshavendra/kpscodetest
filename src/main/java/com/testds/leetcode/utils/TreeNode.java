cd94aa353fffbd1dd37ab11e361a421a62b346d7
keshavendra <kpsingh2607@gmail.com>
2023-09-17 01:11:47 +0530

    Added code for 'maximum-depth-of-binary-tree' problem.

---
+++ src/main/java/com/testds/leetcode/utils/TreeNode.java
@@ -0,0 +1,35 @@
+package com.testds.leetcode.utils;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public static TreeNode createTree(Integer[] nodes) {
+        if (nodes != null && nodes.length > 0) {
+            TreeNode[] nodesArray = new TreeNode[nodes.length];
+            for (int i = nodes.length - 1; i >= 0; i--) {
+                if (nodes[i] != null) {
+                    nodesArray[i] = new TreeNode(nodes[i]);
+                    nodesArray[i].left = i * 2 + 1 < nodesArray.length ? nodesArray[i * 2 + 1] : null;
+                    nodesArray[i].right = i * 2 + 2 < nodesArray.length ? nodesArray[i * 2 + 2] : null;
+                }
+            }
+            return nodesArray[0];
+        }
+        return null;
+    }
+}
