1ff4c2c34b905ed47d9b70d381c9f141b9536e47
keshavendra <kpsingh2607@gmail.com>
2023-12-16 23:27:14 +0530

    Update UT code for 'valid-anagram'.

--- src/test/java/com/testds/leetcode/problems/isAnagram/SolutionTest.java
+++ src/test/java/com/testds/leetcode/problems/isAnagram/SolutionTest.java
@@ -1,4 +1,46 @@
 package com.testds.leetcode.problems.isAnagram;
 
-public class SolutionTest {
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class SolutionTest extends TestCase {
+
+    private final Solution solution = new Solution();
+
+    @Parameterized.Parameter
+    public String s;
+
+    @Parameterized.Parameter(1)
+    public String t;
+
+    @Parameterized.Parameter(2)
+    public boolean expectedResult;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[][]{
+                        {"anagram", "nagaram", true},
+                        {"rat", "car", false}
+                }
+        );
+    }
+
+    public static Test suite() {
+        return new TestSuite(SolutionTest.class);
+    }
+
+    @org.junit.Test
+    public void testIsAnagram() {
+        boolean actualResult = solution.isAnagram(s, t);
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
