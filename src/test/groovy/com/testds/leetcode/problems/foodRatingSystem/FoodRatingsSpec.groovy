3af8769c042881bb49ad722ac972b915bc242e38
keshavendra <kpsingh2607@gmail.com>
2023-12-17 23:19:43 +0530

    Updated code for UT.

--- src/test/groovy/com/testds/leetcode/problems/foodRatingSystem/FoodRatingsSpec.groovy
+++ src/test/groovy/com/testds/leetcode/problems/foodRatingSystem/FoodRatingsSpec.groovy
@@ -20,7 +20,7 @@ class FoodRatingsSpec extends Specification {
                     foodRatings = new FoodRatings(inputToTheAction.get(i).get(0) as String[], inputToTheAction.get(i).get(1) as String[], inputToTheAction.get(i).get(2) as int[])
                     break
                 case "highestRated":
-                    result = foodRatings.highestRated(inputToTheAction.get(i) as String)
+                    result = foodRatings.highestRated(inputToTheAction.get(i))
                     break
                 case "changeRating":
                     foodRatings.changeRating(inputToTheAction.get(i).get(0) as String, inputToTheAction.get(i).get(1) as int)
