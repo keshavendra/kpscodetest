30934b9ee10dffc43298281c02d94ef2b842094b
keshavendra <kpsingh2607@gmail.com>
2024-02-13 13:05:39 +0530

    Updated code for 'majority-element'.

--- src/main/java/com/testds/leetcode/problems/majorityElement/Solution.java
+++ src/main/java/com/testds/leetcode/problems/majorityElement/Solution.java
@@ -1,21 +1,16 @@
 //https://leetcode.com/problems/majority-element/
 package com.testds.leetcode.problems.majorityElement;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> frequencyMap = new HashMap<>();
         Arrays.stream(nums).forEach(x -> frequencyMap.put(x, frequencyMap.getOrDefault(x, 0) + 1));
-        int maxNum = -1, maxFreq = -1;
-        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            if (entry.getValue() > maxFreq) {
-                maxFreq = entry.getValue();
-                maxNum = entry.getKey();
-            }
-        }
-        return maxNum;
+        final Optional<Map.Entry<Integer, Integer>> max = frequencyMap.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue));
+        if (max.isPresent())
+            return max.get().getKey();
+        else
+            return -1;
     }
 }
