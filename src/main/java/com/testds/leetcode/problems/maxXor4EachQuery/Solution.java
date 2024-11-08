dccc909ee99ac030446346cbb1d0111011916f5d
KPS <keshavendra.singh@oracle.com>
2024-11-08 23:15:48 +0530

    Update code for 'maximum-xor-for-each-query'

--- src/main/java/com/testds/leetcode/problems/maxXor4EachQuery/Solution.java
+++ src/main/java/com/testds/leetcode/problems/maxXor4EachQuery/Solution.java
@@ -1,19 +1,7 @@
 //https://leetcode.com/problems/maximum-xor-for-each-query/
 package com.testds.leetcode.problems.maxXor4EachQuery;
 
-import java.util.Arrays;
-
 public class Solution {
-    public static void main(String[] args) {
-//        int[] nums = new int[]{0, 1, 1, 3};
-//        int maximumBit = 2;
-//        int[] nums = new int[]{2, 3, 4, 7};
-//        int maximumBit = 3;
-        int[] nums = new int[]{0, 1, 2, 2, 5, 7};
-        int maximumBit = 3;
-        System.out.println(Arrays.toString(new Solution().getMaximumXor(nums, maximumBit)));
-    }
-
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         int[] resultArray = new int[nums.length];
         int maxNum = (2 << (maximumBit - 1)) - 1;
