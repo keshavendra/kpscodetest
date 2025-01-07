9d12aeff7a8dfbeceb83f419d0e70aa34e75e3db
KPS <keshavendra.singh@oracle.com>
2025-01-07 14:27:59 +0530

    Updated code for 'minimum-number-of-operations-to-move-all-balls-to-each-box'

--- src/main/java/com/testds/leetcode/problems/minOperationsToMakeAll1s/Solution.java
+++ src/main/java/com/testds/leetcode/problems/minOperationsToMakeAll1s/Solution.java
@@ -1,33 +1,23 @@
 //https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
 package com.testds.leetcode.problems.minOperationsToMakeAll1s;
 
-import java.util.Arrays;
-
 public class Solution {
     public int[] minOperations(String boxes) {
-        int[] leftDistance = new int[boxes.length()];
-        int[] _1s = new int[boxes.length()];
-        for (int i = 1; i < boxes.length(); i++) {
-            _1s[i] = (boxes.charAt(i - 1) == '1' ? 1 : 0) + _1s[i - 1];
-            leftDistance[i] = (boxes.charAt(i - 1) == '1' ? 1 : 0)
-                    + leftDistance[i - 1]
-                    + _1s[i - 1];
-        }
+        int[] result = new int[boxes.length()];
 
-        int[] rightDistance = new int[boxes.length()];
-        Arrays.fill(_1s, 0);
-
-        for (int i = boxes.length() - 2; i >= 0; i--) {
-            _1s[i] = (boxes.charAt(i + 1) == '1' ? 1 : 0) + _1s[i + 1];
-            rightDistance[i] = (boxes.charAt(i + 1) == '1' ? 1 : 0)
-                    + rightDistance[i + 1]
-                    + _1s[i + 1];
-        }
+        int left1 = 0, right1 = 0, leftDistance = 0, rightDistance = 0;
 
         for (int i = 0; i < boxes.length(); i++) {
-            rightDistance[i] += leftDistance[i];
+            result[i] += leftDistance;
+            left1 += boxes.charAt(i) == '1' ? 1 : 0;
+            leftDistance += left1;
+
+            int j = boxes.length() - 1 - i;
+            result[j] += rightDistance;
+            right1 += boxes.charAt(j) == '1' ? 1 : 0;
+            rightDistance += right1;
         }
 
-        return rightDistance;
+        return result;
     }
 }
