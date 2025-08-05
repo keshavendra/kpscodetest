19a930ff894fcbc2cefd069e70c54cd9754a2b66
KPS <keshavendra.singh@oracle.com>
2025-08-05 12:51:46 +0530

    Added code for 'fruits-into-baskets-ii'

---
+++ src/main/java/com/testds/leetcode/problems/numOfUnplacedFruits/Solution.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/fruits-into-baskets-ii
+package com.testds.leetcode.problems.numOfUnplacedFruits;
+
+public class Solution {
+
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int placed = 0;
+        int indexFruits = 0, indexBaskets = 0;
+
+        for (; indexFruits < fruits.length; indexFruits++) {
+            for (indexBaskets = 0; indexBaskets < baskets.length; indexBaskets++) {
+                if (fruits[indexFruits] <= baskets[indexBaskets]) {
+                    placed++;
+                    baskets[indexBaskets] = 0;
+                    break;
+                }
+            }
+        }
+
+        return fruits.length - placed;
+    }
+}
