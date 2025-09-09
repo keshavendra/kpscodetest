64c91a4c714887ab41a0536f1052483374ced482
KPS <kpsingh2607@gmail.com>
2025-09-10 00:20:21 +0530

    Added code 'convert-integer-to-the-sum-of-two-no-zero-integers'

---
+++ src/test/java/com/testds/leetcode/problems/getNoZeroIntegers/SolutionTest.java
@@ -0,0 +1,55 @@
+package com.testds.leetcode.problems.getNoZeroIntegers;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class SolutionTest extends TestCase {
+
+    private Solution solution;
+
+    @Parameterized.Parameter
+    public int n;
+
+    @Parameterized.Parameter(1)
+    public int[] expectedResult;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(
+                new Object[][]{
+                        {2, new int[]{1, 1}},
+                        {11, new int[]{2, 9}}
+                }
+        );
+    }
+
+    public static Test suite() {
+        return new TestSuite(SolutionTest.class);
+    }
+
+    @Before
+    public void setUp() {
+        solution = new Solution();
+    }
+
+    @org.junit.Test
+    public void testNoZeroIntegers() {
+        Assert.assertArrayEquals(expectedResult, solution.getNoZeroIntegers(n));
+    }
+
+    @After
+    public void tearDown() {
+        solution = null;
+    }
+
+}
