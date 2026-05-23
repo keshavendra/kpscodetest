641f2d25a45f80af15b4f7063694ebb066e61e93
KPS <kpsingh2607@gmail.com>
2026-05-23 10:31:27 +0530

    Updated the record to class to validate the readObject and writeObject behaviour

--- src/main/java/com/testSerialization/pojo/ShoppingCart.java
+++ src/main/java/com/testSerialization/pojo/ShoppingCart.java
@@ -1,9 +1,21 @@
 package com.testSerialization.pojo;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
-public record ShoppingCart(int id, List<Item> items) implements Serializable {
+public class ShoppingCart implements Serializable {
+
+    private int id;
+    private final List<Item> items;
+
+    public ShoppingCart(int id, List<Item> items) {
+        this.id = id;
+        this.items = items;
+    }
 
     public void addItem(Item item) {
         items.add(item);
@@ -19,6 +31,25 @@ public record ShoppingCart(int id, List<Item> items) implements Serializable {
 
     public int getCartSize() {
         return items.size();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        Date date = new Date();
+        System.out.println("Serializing date as : " + date);
+        out.writeObject(date);
+    }
+
+    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        System.out.println(this);
+        System.out.println("reading shopping cart from file");
+        ois.defaultReadObject();
+        System.out.println("The date Field : " + ois.readObject());
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart [id=" + id + ", items=" + items + "]";
     }
 
 }
