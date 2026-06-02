d192fbb96f294bed73e8d3b12b824c420dcc68cc
KPS <kpsingh2607@gmail.com>
2026-06-03 00:34:59 +0530

    Updated code for 'earliest-finish-time-for-land-and-water-rides-i'.

--- src/main/java/com/testds/leetcode/problems/earliestFinishTime/Solution.java
+++ src/main/java/com/testds/leetcode/problems/earliestFinishTime/Solution.java
@@ -3,39 +3,37 @@ package com.testds.leetcode.problems.earliestFinishTime;
 
 public class Solution {
 
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int leastFinishTime = Integer.MAX_VALUE;
-        for (int cand = 0; cand < landStartTime.length; cand++) {
-            for (int fin = 0; fin < waterStartTime.length; fin++) {
-                //if land is taken first
-                if (waterStartTime[fin] >= (landStartTime[cand] + landDuration[cand])) {
-                    if (leastFinishTime > waterStartTime[fin] + waterDuration[fin]) {
-                        leastFinishTime = waterStartTime[fin] + waterDuration[fin];
-                    }
-                } else {
-                    if (leastFinishTime > (landStartTime[cand] + landDuration[cand] + waterDuration[fin])) {
-                        leastFinishTime = landStartTime[cand] + landDuration[cand] + waterDuration[fin];
-                    }
-                }
-            }
+    public int earliestFinishTime(
+            int[] landStartTime,
+            int[] landDuration,
+            int[] waterStartTime,
+            int[] waterDuration
+    ) {
+        int minLandFinish = Integer.MAX_VALUE;
+        int minWaterFinish = Integer.MAX_VALUE;
+
+        for (int i = 0; i < landStartTime.length; i++) {
+            minLandFinish = Math.min(minLandFinish, landStartTime[i] + landDuration[i]);
         }
 
-
-        for (int cand = 0; cand < waterStartTime.length; cand++) {
-            for (int fin = 0; fin < landStartTime.length; fin++) {
-                //if water is taken first
-                if (landStartTime[fin] >= (waterStartTime[cand] + waterDuration[cand])) {
-                    if (leastFinishTime > landStartTime[fin] + landDuration[fin]) {
-                        leastFinishTime = landStartTime[fin] + landDuration[fin];
-                    }
-                } else {
-                    if (leastFinishTime > (waterStartTime[cand] + waterDuration[cand] + landDuration[fin])) {
-                        leastFinishTime = (waterStartTime[cand] + waterDuration[cand] + landDuration[fin]);
-                    }
-                }
-            }
+        for (int i = 0; i < waterStartTime.length; i++) {
+            minWaterFinish = Math.min(minWaterFinish, waterStartTime[i] + waterDuration[i]);
         }
-        return leastFinishTime;
+
+        int ans = Integer.MAX_VALUE;
+
+        // Land first, then water
+        for (int i = 0; i < waterStartTime.length; i++) {
+            int finish = Math.max(minLandFinish, waterStartTime[i]) + waterDuration[i];
+            ans = Math.min(ans, finish);
+        }
+
+        // Water first, then land
+        for (int i = 0; i < landStartTime.length; i++) {
+            int finish = Math.max(minWaterFinish, landStartTime[i]) + landDuration[i];
+            ans = Math.min(ans, finish);
+        }
+
+        return ans;
     }
-
 }
